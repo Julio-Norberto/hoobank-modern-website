f56a923e98e83927e098a7796edce23a603e6ba2
Julio-Norberto <juliocsscl@gmail.com>
2022-12-04 19:08:01 -0300

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import React from 'react'
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
