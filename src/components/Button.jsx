99b62ad8b9a70dd6c7a53f38bdd35d3debed59ac
Julio-Norberto <juliocsscl@gmail.com>
2022-12-04 22:48:34 -0300

    create Business

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Button() {
+export default function Button({ styles }) {
     return (
-        <div>
-            <p>Billing</p>
-        </div>
+        <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+            Get Started
+        </button>
     )
 }
